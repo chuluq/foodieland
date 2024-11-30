6430c746d41678d246562e12294e5e81cd25bae7
chuluq <chuluq@arvis.id>
2024-11-30 21:46:24 +0700

    feat: add carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
 import { Footer } from "@/components/footer";
 import { Header } from "@/components/header";
 import { FeaturedRecipe } from "@/components/recipe/featured";
@@ -6,7 +11,17 @@ export default function Home() {
   return (
     <div className="font-sans">
       <Header />
-      <FeaturedRecipe />
+      <div className="px-10 pt-10">
+        <Carousel>
+          <CarouselContent className="-ml-10">
+            {Array.from({ length: 5 }).map((_, index) => (
+              <CarouselItem key={index} className="pl-10">
+                <FeaturedRecipe />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
+      </div>
       <Footer />
     </div>
   );
--- src/components/recipe/featured.tsx
+++ src/components/recipe/featured.tsx
@@ -8,71 +8,60 @@ import hotRecipe from "@/assets/images/hot-recipe.png";
 
 export const FeaturedRecipe = () => {
   return (
-    <div className="px-20 pt-10">
-      <div className="flex h-[640px] w-full">
-        <div className="flex flex-col rounded-l-2xl bg-[#E7FAFE] p-[50px]">
-          <div className="space-y-8">
-            <div className="flex w-fit items-center gap-3 rounded-2xl bg-white px-5 py-[10px] drop-shadow-sm">
-              <Image src={hotRecipe} alt="hot-recipe" width={24} height={24} />
-              <p className="text-sm font-semibold capitalize text-black">
-                Hot Recipes
+    <div className="flex h-[640px] w-full">
+      <div className="flex flex-col rounded-l-2xl bg-[#E7FAFE] p-[50px]">
+        <div className="space-y-8">
+          <div className="flex w-fit items-center gap-3 rounded-2xl bg-white px-5 py-[10px] drop-shadow-sm">
+            <Image src={hotRecipe} alt="hot-recipe" width={24} height={24} />
+            <p className="text-sm font-semibold capitalize text-black">
+              Hot Recipes
+            </p>
+          </div>
+          <div className="space-y-[30px]">
+            <div className="space-y-6">
+              <h1 className="text-[64px] font-semibold text-black">
+                Spicy delicious chicken wings
+              </h1>
+              <p className="text-base leading-7 text-black/60">
+                Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqut enim
+                ad minim{" "}
               </p>
             </div>
-            <div className="space-y-[30px]">
-              <div className="space-y-6">
-                <h1 className="text-[64px] font-semibold text-black">
-                  Spicy delicious chicken wings
-                </h1>
-                <p className="text-base leading-7 text-black/60">
-                  Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do
-                  eiusmod tempor incididunt ut labore et dolore magna aliqut
-                  enim ad minim{" "}
-                </p>
-              </div>
-              <div className="flex items-center gap-4">
-                <div className="flex items-center gap-[10px] rounded-2xl bg-black/5 px-4 py-2">
-                  <Timer size={24} color="black" />
-                  <p className="text-sm font-medium capitalize text-black/60">
-                    30 Minutes
-                  </p>
-                </div>
-                <div className="flex items-center gap-[10px] rounded-2xl bg-black/5 px-4 py-2">
-                  <Utensils size={24} color="black" />
-                  <p className="text-sm font-medium capitalize text-black/60">
-                    Chicken
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="mt-auto flex items-center justify-between">
             <div className="flex items-center gap-4">
-              <Avatar>
-                <AvatarImage
-                  src="https://github.com/shadcn.png"
-                  alt="@shadcn"
-                />
-                <AvatarFallback>CN</AvatarFallback>
-              </Avatar>
-              <div className="space-y-2">
-                <p className="font-bold">John Smith</p>
-                <p className="text-sm font-medium text-black/60">
-                  15 March 2022
+              <div className="flex items-center gap-[10px] rounded-2xl bg-black/5 px-4 py-2">
+                <Timer size={24} color="black" />
+                <p className="text-sm font-medium capitalize text-black/60">
+                  30 Minutes
+                </p>
+              </div>
+              <div className="flex items-center gap-[10px] rounded-2xl bg-black/5 px-4 py-2">
+                <Utensils size={24} color="black" />
+                <p className="text-sm font-medium capitalize text-black/60">
+                  Chicken
                 </p>
               </div>
             </div>
-            <Button className="h-[60px] space-x-4 rounded-2xl px-9 capitalize">
-              View Recipes
-              <PlayCircle size={24} />
-            </Button>
           </div>
         </div>
-        <Image
-          src={bakedChicken}
-          alt="baked-chicken"
-          className="rounded-r-2xl"
-        />
+        <div className="mt-auto flex items-center justify-between">
+          <div className="flex items-center gap-4">
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            <div className="space-y-2">
+              <p className="font-bold">John Smith</p>
+              <p className="text-sm font-medium text-black/60">15 March 2022</p>
+            </div>
+          </div>
+          <Button className="h-[60px] space-x-4 rounded-2xl px-9 font-semibold capitalize">
+            View Recipes
+            <PlayCircle size={24} />
+          </Button>
+        </div>
       </div>
+      <Image src={bakedChicken} alt="baked-chicken" className="rounded-r-2xl" />
     </div>
   );
 };
