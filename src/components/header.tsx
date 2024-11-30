3e9742d30ad960b8a28f25f0a72e30d53f59c40a
chuluq <chuluq@arvis.id>
2024-11-30 20:24:10 +0700

    style: add line

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,36 +1,40 @@
-import Link from "next/link"
-import Facebook from "@/assets/facebook.svg"
-import Instagram from "@/assets/instagram.svg"
-import Twitter from "@/assets/x.svg"
+import Link from "next/link";
+import Facebook from "@/assets/facebook.svg";
+import Instagram from "@/assets/instagram.svg";
+import Twitter from "@/assets/x.svg";
 
-import { landingConfig } from "@/config/landing"
+import { landingConfig } from "@/config/landing";
+import { Separator } from "@/components/ui/separator";
 
 export const Header = () => {
   return (
-    <header className="px-20 py-10 flex justify-between items-center">
-      <h1 className="font-lobster text-2xl text-black">Foodieland.</h1>
-      <div className="flex items-center gap-[60px]">
-        {landingConfig.mainNav.map((nav, index) => (
-          <Link
-            key={index}
-            href={nav.href}
-            className="font-medium text-base capitalize"
-          >
-            {nav.title}
-          </Link>
-        ))}
-      </div>
-      <div className="flex items-center gap-10">
-        <div className="size-5">
-          <Facebook />
+    <header className="flex flex-col">
+      <nav className="flex items-center justify-between px-20 py-10">
+        <h1 className="font-lobster text-2xl text-black">Foodieland.</h1>
+        <div className="flex items-center gap-[60px]">
+          {landingConfig.mainNav.map((nav, index) => (
+            <Link
+              key={index}
+              href={nav.href}
+              className="text-base font-medium capitalize"
+            >
+              {nav.title}
+            </Link>
+          ))}
         </div>
-        <div className="size-5">
-          <Twitter />
+        <div className="flex items-center gap-10">
+          <div className="size-5">
+            <Facebook />
+          </div>
+          <div className="size-5">
+            <Twitter />
+          </div>
+          <div className="size-5">
+            <Instagram />
+          </div>
         </div>
-        <div className="size-5">
-          <Instagram />
-        </div>
-      </div>
+      </nav>
+      <Separator className="border-black/10" />
     </header>
-  )
-}
+  );
+};
